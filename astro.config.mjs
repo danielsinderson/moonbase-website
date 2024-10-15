62f9a9ce0dedc65300631ca402872109dedc5e38
drsinderson <drsinderson@gmail.com>
2024-10-14 21:11:31 -0700

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://moon-base-books.com',
+  integrations: [sitemap()]
+});
